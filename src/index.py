b82d4802ac8953858b2783d6eb7a66f0b97a6272
hejianglin <hejianglin@uino.com>
2023-09-03 19:54:17 +0800

    fix: 添加解析

--- src/index.py
+++ src/index.py
@@ -1,20 +1,43 @@
 from fastapi import FastAPI
-
+from douyin_tiktok_scraper.scraper import Scraper
 from src.dtos.ISayHelloDto import ISayHelloDto
 
 app = FastAPI()
+api = Scraper()
+
+
+def can_download(result):
+    try:
+        nwm_video_url_HQ = result['video_data']['nwm_video_url_HQ']
+        return nwm_video_url_HQ
+    except Exception as e:
+        return None
+
+
+async def hybrid_parsing(url: str):
+    # Hybrid parsing(Douyin/TikTok URL)
+    result = await api.hybrid_parsing(url)
+    print(result['status'])
+    if result['status'] == 'success':
+        video_url = can_download(result)
+        if video_url == None:
+            return '视频主没有开放下载权限'
+        else:
+            return video_url
+    else:
+        return '解析失败'
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "server is running"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/hello")
-async def hello_message(dto: ISayHelloDto):
-    return {"message": f"Hello {dto.message}"}
+@app.get("/tiktok")
+async def tiktok(url: str):
+    try:
+        print(url)
+        result = await hybrid_parsing(url=url)
+        return result
+    except Exception as e:
+        return {"error": str(e)}
