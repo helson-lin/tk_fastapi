f0390a326e737cabcf3744c1f3ffecb684e326e7
hejianglin <hejianglin@uino.com>
2023-09-03 21:46:28 +0800

    fix: 修改接口

--- src/index.py
+++ src/index.py
@@ -20,15 +20,18 @@ def can_download(result):
 
 async def hybrid_parsing(url: str):
     # Hybrid parsing(Douyin/TikTok URL)
-    result = await api.hybrid_parsing(url)
-    print(result['status'])
-    if result['status'] == 'success':
-        video_url = can_download(result)
-        if video_url is None:
-            return 1
+    try:
+        result = await api.hybrid_parsing(url)
+        print(result['status'])
+        if result['status'] == 'success':
+            video_url = can_download(result)
+            if video_url is None:
+                return 1
+            else:
+                return video_url
         else:
-            return video_url
-    else:
+            return 0
+    except Exception as e:
         return 0
 
 
@@ -37,14 +40,14 @@ async def root():
     return {"message": "server is running"}
 
 
-@app.post("/tiktok")
-async def tiktok(dto: TikTikDto):
+@app.get("/tiktok")
+async def tiktok(url: str):
     try:
         result_map = {
             1: '视频主没有开放下载权限',
             0: '解析失败'
         }
-        result = await hybrid_parsing(dto.url)
+        result = await hybrid_parsing(url)
         if result_map[result]:
             return {'code': 1, 'msg': result_map[result], 'data': None}
         else:
